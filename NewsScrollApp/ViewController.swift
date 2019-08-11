84f1e77853274316099154207c9844e148e5fe38
原田悠嗣 <bikkirisaburou@gmail.com>
2019-08-11 10:02:56 +0900

    first Commit

---
+++ NewsScrollApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsScrollApp
+//
+//  Created by 原田悠嗣 on 2019/08/11.
+//  Copyright © 2019 原田悠嗣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
